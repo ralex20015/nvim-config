f73311c4b903613693d62f3744b52bb788d28af5
Alex Robles <lozanoeduardo8777@gmail.com>
2024-09-13 15:18:26 -0600

    Change the plugin manager to use

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"mdkir", "-p",
+		vim.fn.stdpath("data") .. "lazy"
+	})
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypth
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 require('plugins')
 
 require('lsp')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,8 @@
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'  -- Packer puede gestionar itself
-  use 'neovim/nvim-lspconfig'   -- Plugin para LSP
-  use 'morhetz/gruvbox'
-  use 'ThePrimeagen/vim-be-good'
-  use 'mhartington/formatter.nvim'
-  use 'lukas-reineke/indent-blankline.nvim'
-end)
+require('lazy').setup({
+	'wbthomason/packer.nvim',
+	'neovim/nvim-lspconfig',
+	'morhetz/gruvbox',
+	'ThePrimeagen/vim-be-good',
+	'mhartington/formatter.nvim',
+	'lukas-reineke/indent-blankline.nvim'
+})
